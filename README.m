3144db2229d33561ad7446c99b2ba6fb3ef64290
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-01-31 23:49:40 +0530

    Solution

---
+++ README.m
@@ -0,0 +1,20 @@
+# sum-of-digits
+forloop
+
+Program Explanation:
+1} User must first enter the value and store it in a variable.
+2} The for loop is used and the last digit of the number is obtained by using the modulus operator.
+3} The digit is added to another variable each time the loop is executed.
+4} This loop terminates when the value of the number is 0.
+5} The total sum of the number is then printed.
+
+Example 1:
+Enter a number:4543
+Sum of digits= 16
+
+Example 2:
+Enter a number:984
+Sum of digits= 21
+Example 3:
+Enter a number:12
+Sum of digits= 3
